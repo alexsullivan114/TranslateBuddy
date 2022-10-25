6359523b2acbc3bd771019ffaabb7bec3db1c767
Alex Sullivan <alexsullivan114@gmail.com>
2022-10-25 18:15:18 -0400

    Initial commit

---
+++ app/src/main/java/com/alexsullivan/translatebuddy/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.alexsullivan.translatebuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alexsullivan.translatebuddy.ui.theme.TranslateBuddyTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      TranslateBuddyTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}

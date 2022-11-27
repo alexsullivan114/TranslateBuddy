9d1c2888f4597b00c4fd9af8b95b8809391442c9
Alex Sullivan <alexsullivan114@gmail.com>
2022-11-27 10:15:54 -0500

    Add the list of translations to the main screen

--- app/src/main/java/com/alexsullivan/translatebuddy/LaunchActivity.kt
+++ app/src/main/java/com/alexsullivan/translatebuddy/LaunchActivity.kt
@@ -6,11 +6,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.alexsullivan.translatebuddy.drive.GetTranslationsUseCase
+import com.alexsullivan.translatebuddy.drive.Translation
+import com.alexsullivan.translatebuddy.storage.TranslationBuddyPreferences
 import com.alexsullivan.translatebuddy.work.DownloadTranslationsWork
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -25,10 +38,32 @@ class LaunchActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Text("Signing in...")
+      TranslationList()
     }
 
     signIn()
+  }
+
+  @Composable
+  private fun TranslationList() {
+    val translations = TranslationBuddyPreferences(applicationContext).getTranslations()
+    LazyColumn(
+      contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+      verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+      items(translations) { translation ->
+        TranslationItem(translation)
+        Divider()
+      }
+    }
+  }
+
+  @Composable
+  private fun TranslationItem(translation: Translation) {
+    Text(
+      text = "${translation.english} -> ${translation.nepali}",
+      style = TextStyle(fontSize = 24.sp)
+    )
   }
 
   private fun isUserSignedIn(): Boolean {
